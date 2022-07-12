54331019dafd8d2732f3b29f75a2df5c64f4edd7
pvtka <93162478+pvtka@users.noreply.github.com>
2022-07-12 17:25:21 +0200

    DonationController

---
+++ src/main/java/pl/coderslab/charity/Controller/DonationController.java
@@ -0,0 +1,19 @@
+package pl.coderslab.charity.Controller;
+
+import org.springframework.stereotype.Controller;
+import pl.coderslab.charity.repository.CategoryRepository;
+import pl.coderslab.charity.repository.DonationRepository;
+import pl.coderslab.charity.repository.InstitutionRepository;
+
+@Controller
+public class DonationController {
+    private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
+    private final CategoryRepository categoryRepository;
+
+    public DonationController(InstitutionRepository institutionRepository, DonationRepository donationRepository, CategoryRepository categoryRepository) {
+        this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
+        this.categoryRepository = categoryRepository;
+    }
+}
---
+++ src/main/webapp/resources/sql/db.sql
@@ -0,0 +1,5 @@
+
+#database
+
+INSERT INTO institution (description, name) VALUES ('Cel i misja: Pomoc dzieciom z ubogich rodzin.', 'Fundacja: Dbam o Zdrowie');
+INSERT INTO donation (city, pick_up_comment, pick_up_date, pick_up_time, quantity, street, zip_code, institution_id) VALUES ('Krakow', 'be punctual', '2022-07-01', '12:00:00', 3, 'Wroclawska', '30-011', 1);
